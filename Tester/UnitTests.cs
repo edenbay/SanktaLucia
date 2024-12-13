600c13bb6d57b3f2eab805ca75ec9d06ac50c2c6
edenbay <deba2301@student.miun.se>
2024-12-13 10:27:46 +0100

    Add project files.

---
+++ Tester/UnitTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uppgifter;
+
+namespace Tester
+{
+    public class UnitTests
+    {
+        [Theory]
+        [InlineData(3, "March")]
+        [InlineData(12, "December")]
+        [InlineData(6, "June")]
+        [InlineData(11, "November")]
+        public void MonthNameTest(int monthNumber, string expected)
+        {
+            string result = Program.MonthName(monthNumber);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(2, 7200)]
+        [InlineData(10, 36000)]
+        [InlineData(24, 86400)]
+        public void HowManySecondsTest(int hours, int expected)
+        {
+            int result = Program.HowManySeconds(hours);
+
+            Assert.Equal(expected, result);
+        }
+
+
+        [Theory]
+        [InlineData(3, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(4, "4")]
+        public void FizzBuzzTest(int number, string expected)
+        {
+            string result = Program.FizzBuzz(number);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Uppgifter/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Uppgifter
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            //Provkör gärna. För att få se ditt resultat använder du antingen
+            //breakpoints eller Console.WriteLine()
+        }
+
+
+        //Create a function that takes a number(from 1 to 12) and returns its corresponding month name as a string. 
+        //For example, if you're given 3 as input, your function should return "March", because March is the 3rd month.
+        public static string MonthName(int monthNumber)
+        {
+            return default;
+        }
+
+        //Write a function that converts hours into seconds.
+        //60 seconds in a minute, 60 minutes in an hour.
+        //Don't forget to return your answer.
+        public static int HowManySeconds(int hours)
+        {
+            return default;
+        }
+
+        //Create a function that takes a number as an argument and returns "Fizz", "Buzz" or "FizzBuzz".
+        //    If the number is a multiple of 3 the output should be "Fizz".
+        //    If the number given is a multiple of 5, the output should be "Buzz".
+        //    If the number given is a multiple of both 3 and 5, the output should be "FizzBuzz".
+        //    If the number is not a multiple of either 3 or 5, the number should be output on its own as shown in the examples below.
+        //    The output should always be a string even if it is not a multiple of 3 or/and 5.
+        public static string FizzBuzz(int number)
+        {
+            return default;
+
+        }
+    }
+}
